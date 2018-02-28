7c7699f6dce4a57b2793de2a502f902a7f018833
faithfem <fmugure@intracitygeeks.org>
2018-02-27 19:59:52 -0500

    imdbScrapper

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -16,6 +16,8 @@ public class Scrapper {
         for (Element row : document.select("table.chart.full-width tr")){
             //First we want the title from the title column
             final String title = row.select(".titleColumn").text(); //"dot titleColumn" coz it's a class
+            //First: IN CASE YOU DON'T WANT THE TITLES NUMBERED 1 TO 250, COPY ONLY THE HREF
+            //final String title = row.select(".titleColumn a").text(); //"dot titleColumn" coz it's a class
             //Second: we want the imdb rating
             final String rating = row.select(".imdbRating").text();
 
