515684374f9df7e6af8b0661d59bf9f3301d75fe
faithfem <fmugure@intracitygeeks.org>
2018-02-27 19:54:01 -0500

    imdbScrapper

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -1,7 +1,26 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+
 public class Scrapper {
 
     //Add a main method to this class
-    public static void main(String[]args){
+    public static void main(String[]args) throws Exception{
+
+        final Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
+
+        //System.out.println(document.outerHtml());
+
+        for (Element row : document.select("table.chart.full-width tr")){
+            //First we want the title from the title column
+            final String title = row.select(".titleColumn").text(); //"dot titleColumn" coz it's a class
+            //Second: we want the imdb rating
+            final String rating = row.select(".imdbRating").text();
+
+            System.out.println(title + " -> Rating: " + rating);
+        }
 
     }
 
