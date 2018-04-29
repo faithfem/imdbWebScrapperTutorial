605c1cff8084b746ecccc7d5d99783381db58cb4
faithfem <fmugure@intracitygeeks.org>
2018-04-29 19:25:00 -0400

    finance

---
+++ Craigslist/src/Piano.java
@@ -0,0 +1,12 @@
+public class Piano {
+
+    String searchQuery = "Piano";
+    WebClient client = new WebClient();
+    client.getOptions().setCssEnabled(false);
+    client.getOptions().setJavaScriptEnabled(false);
+
+    try{
+        String searchUrl = "https://providence.craigslist.org/search/sss?query=piano&sort=rel"
+
+    }
+}
